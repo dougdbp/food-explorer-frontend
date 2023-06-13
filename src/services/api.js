f1ffde516a7b03f208b65d04c717ac9a7310cec1
douglas <38635101+dougdbp@users.noreply.github.com>
2023-06-13 19:14:55 -0300

    baseURL API update

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-   baseURL: 'http://localhost:3333',
-  // baseURL: 'https://',
+   // baseURL: 'http://localhost:3333',
+  baseURL: 'https://foodexplorer-lw32.onrender.com:3333',
 });
